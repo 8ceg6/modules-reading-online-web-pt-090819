ea679119714ab58c02ed4d3a537b2d61bfb4b0fb
cedric e goddard <8ceg6@comcast.net>
2019-10-18 19:27:03 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,10 +6,10 @@ class Dancer
   extend FancyDance::ClassMethods
   include 
   FancyDance::InstanceMethods
-  # include Dance 
-  # attr_accessor :name 
+  include Dance 
+  attr_accessor :name 
   
-  # def initialize(name)
-  #   @name = name
-  # end 
+  def initialize(name)
+    @name = name
+  end 
 end 
